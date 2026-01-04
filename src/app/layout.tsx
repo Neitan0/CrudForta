70810d32f0d8477d36edaa51eea54841d0aa1b65
Natanael <natan.nael1258@gmail.com>
2026-01-04 12:26:33 -0300

    feat: resposividade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Viewport } from 'next'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,4 +32,11 @@ export default function RootLayout({
       </body>
     </html>
   );
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1, // Isso aqui impede o zoom ao focar no input
+  userScalable: false,
 }
